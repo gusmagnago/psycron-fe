5024e7d514a43490b4b28d9c6693dc2588f97a4d
Gustavo Magnago <ext.gustavo.magnago@farfetch.com>
2024-05-29 17:10:33 +0300

    feat: Icon components

---
+++ src/components/icons/Icons.stories.tsx
@@ -0,0 +1,78 @@
+import styled from '@emotion/styled';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import * as Icons from './index';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const IconsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  margin-bottom: 40px;
+`;
+
+const ListItemWrapper = styled.div`
+  margin: 10px;
+  text-align: center;
+  height: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  & svg {
+    width: 30px;
+    height: auto;
+  }
+`;
+
+const IconList = () => {
+	const iconsArray = Object.entries(Icons);
+	const logoIcon = iconsArray.filter(([name]) => name === 'Logo');
+	const otherIcons = iconsArray.filter(([name]) => name !== 'Logo');
+
+	return (
+		<Wrapper>
+			{logoIcon && (
+				<IconsWrapper>
+					{logoIcon.map(([name, Icon]) => (
+						<ListItemWrapper key={name}>
+							<Icon />
+							<div>{name}</div>
+						</ListItemWrapper>
+					))}
+				</IconsWrapper>
+				
+			)}
+			<IconsWrapper>
+				{otherIcons.map(([name, Icon]) => (
+					<ListItemWrapper key={name}>
+						<Icon />
+						<div>{name}</div>
+					</ListItemWrapper>
+				))}
+			</IconsWrapper>
+		</Wrapper>
+	);
+};
+
+const meta: Meta = {
+	title: 'Icons',
+	component: IconList,
+};
+
+export default meta;
+
+type Story = StoryObj;
+
+export const Default: Story = {
+	render: () => <IconList />,
+};
---
+++ src/theme/globalStyles.tsx
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+import { GlobalStyles } from '@mui/material';
+
+const globalStyles = (
+	<GlobalStyles
+		styles={css`
+      .Mui-disabled {
+        cursor: not-allowed !important;
+      }
+
+        .disabled {
+            cursor: not-allowed !important;
+
+      }
+    `}
+	/>
+);
+
+export default globalStyles;
