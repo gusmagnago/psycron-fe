23364475eb9b1826f343f74ae2f376484a6a1d81
Gustavo Magnago <ext.gustavo.magnago@farfetch.com>
2024-07-31 08:27:16 +0300

    fix: Localization get from browser

--- src/components/localization/Localization.tsx
+++ src/components/localization/Localization.tsx
@@ -4,6 +4,8 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { StyledSelectWrapper } from './Localization.styles';
 import { Select } from '@psycron/components/select/Select';
 
+const LANGKEY = 'i18nextLng';
+
 export const Localization = () => {
   const { i18n } = useTranslation();
   const { lang } = useParams<{ lang: string }>();
@@ -15,9 +17,25 @@ export const Localization = () => {
     { name: 'PT', value: 'pt' },
   ];
 
+  const getBrowserLang = () => {
+    const browserLang = navigator.languages
+      ? navigator.languages[0]
+      : navigator.language;
+    return browserLang.split('-')[0].toLowerCase();
+  };
+
   const getDefaultLang = () => {
-    const storedLang = localStorage.getItem('i18nextLng');
-    return storedLang ? storedLang.toLowerCase() : lang?.toLowerCase() || 'en';
+    const storedLang = localStorage.getItem(LANGKEY);
+    if (storedLang) {
+      return storedLang.toLowerCase();
+    } else {
+      const browserLang = getBrowserLang();
+      if (availableLanguages.some((lang) => lang.value === browserLang)) {
+        return browserLang;
+      } else {
+        return 'en';
+      }
+    }
   };
 
   const [defaultLang, setDefaultLang] = useState<string>(getDefaultLang());
@@ -26,14 +44,14 @@ export const Localization = () => {
     const newLang = lng.toLowerCase();
     i18n.changeLanguage(newLang);
     setDefaultLang(newLang);
-    localStorage.setItem('i18nextLng', newLang);
+    localStorage.setItem(LANGKEY, newLang);
 
     const newPath = location.pathname.replace(`/${lang}`, `/${newLang}`);
     navigate(newPath);
   };
 
   useEffect(() => {
-    const storedLang = localStorage.getItem('i18nextLng');
+    const storedLang = localStorage.getItem(LANGKEY);
     if (storedLang && i18n.language !== storedLang) {
       changeLanguage(storedLang.toLowerCase());
     } else if (lang && i18n.language !== lang) {
@@ -44,7 +62,6 @@ export const Localization = () => {
   return (
     <StyledSelectWrapper>
       <Select
-        selectLabel={defaultLang.toUpperCase()}
         value={defaultLang}
         onChangeSelect={(e) => changeLanguage(e.target.value as string)}
         items={availableLanguages}
